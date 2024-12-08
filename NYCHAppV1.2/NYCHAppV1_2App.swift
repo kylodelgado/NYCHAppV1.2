381f194f8ae8292a9a6a99f5f4d79d69f3b8c23d
Brandon Delgado <kylo.delgado@gmail.com>
2024-12-07 22:55:16 -0500

    Initial Commit

---
+++ NYCHAppV1.2/NYCHAppV1_2App.swift
@@ -0,0 +1,17 @@
+//
+//  NYCHAppV1_2App.swift
+//  NYCHAppV1.2
+//
+//  Created by Brandon Delgado on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct NYCHAppV1_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NYCHAppV1.2Tests/NYCHAppV1_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  NYCHAppV1_2Tests.swift
+//  NYCHAppV1.2Tests
+//
+//  Created by Brandon Delgado on 12/7/24.
+//
+
+import Testing
+@testable import NYCHAppV1_2
+
+struct NYCHAppV1_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
